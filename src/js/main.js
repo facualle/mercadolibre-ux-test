0305798841269f5dea18c3787576c5007c635aeb
Facundo Allemand <fallemand@simtlix.com>
2015-10-26 01:51:15 -0300

    Added comments.

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,15 @@
+/*!
+ * MercadoLibre UX Test v1.0
+ * https://github.com/facualle/mercadolibre-ux-test/
+ *
+ * author: Facundo Allemand
+ * http://www.fallemand.com
+ */
+
 $(window).on("load", function () {
     
     // Create a new carousel.
-    var carousel = new ch.Carousel($('.ch-carousel'), null);
+    var carousel = new ch.Carousel($('.ch-carousel'));
     
     // Add tooltip to similar products
     var tooltips = $('[data-toggle=tooltip]').tooltip({
